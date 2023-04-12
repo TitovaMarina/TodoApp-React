7da980265bc45e4f99cdd20814dfadd00d7bb922
Marina Titova <Marina.Titova2@icl-services.com>
2023-04-12 19:13:58 +0300

    fixed empty todo list case

--- src/App.tsx
+++ src/App.tsx
@@ -5,65 +5,80 @@ import { TodoList } from './components/Todos/TodoList';
 import { TodoPanel } from './components/Todos/TodoPanel';
 import { Wrapper } from './components/Wrapper/Wrapper';
 
-const DEFAULT_TODOS = [{
-  id: 1,
-  title: 'Write React app',
-  checked: false
-}]
+const DEFAULT_TODOS = [
+  {
+    id: 1,
+    title: 'Write React app',
+    checked: false,
+  },
+];
 
 export const App: React.FC = () => {
   const [todos, setTodos] = React.useState(DEFAULT_TODOS);
   const [todoIdForEdit, setTodoIdForEdit] = React.useState<number | null>(null);
 
   //for debug
-  React.useEffect(() => {    
-    console.log('todos=',todos);
+  React.useEffect(() => {
+    console.log('todos=', todos);
   }, [todos]);
 
   const selectTodoIdForEdit = (id: number) => {
     setTodoIdForEdit(id);
-  }
+  };
 
   const addTodo = (title: string) => {
-    setTodos([...todos, { id: todos[todos.length - 1].id + 1, title: title, checked: false }]);
+    const id = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+
+    setTodos([...todos, { id, title, checked: false }]);
   };
 
   const deleteTodo = (id: Todo['id']) => {
     setTodos(todos.filter((todo) => todo.id !== id));
-  }
+  };
 
   const checkTodo = (id: Todo['id']) => {
-    setTodos(todos.map(todo => {
-      if (todo.id === id){
-        return { ...todo, checked: !todo.checked};
-      }
-      return todo;
-    }));
-  }
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, checked: !todo.checked };
+        }
+        return todo;
+      })
+    );
+  };
 
-  const editTodo = (title: string) => {        
-    setTodos(todos.map(todo => {
-      if (todo.id === todoIdForEdit){
-        return { ...todo, title};
-      }
-      return todo;
-    }));
+  const editTodo = (title: string) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === todoIdForEdit) {
+          return { ...todo, title };
+        }
+        return todo;
+      })
+    );
     setTodoIdForEdit(null);
-  }
+  };
 
   return (
     <div className="App">
-      <Header todoCount={todos.length}/>
+      <Header todoCount={todos.length} />
       <Wrapper color="blue" direction="row">
-        <TodoPanel mode="add" onClick={addTodo} todoTitle=""/>
+        <TodoPanel mode="add" onClick={addTodo} todoTitle="" />
       </Wrapper>
-      <TodoList 
-        todos={todos} 
-        todoIdForEdit = {todoIdForEdit}
-        checkTodo={checkTodo} 
-        deleteTodo={deleteTodo} 
-        changeTodo={editTodo}
-        selectTodoIdForEdit={selectTodoIdForEdit}/>
+      {todos.length > 0 ? (
+        <TodoList
+          todos={todos}
+          todoIdForEdit={todoIdForEdit}
+          checkTodo={checkTodo}
+          deleteTodo={deleteTodo}
+          changeTodo={editTodo}
+          selectTodoIdForEdit={selectTodoIdForEdit}
+        />
+      ) : (
+        <Wrapper color="green" direction="column">
+          You have no tasks.
+        </Wrapper>
+      )}
     </div>
   );
 };
